b531c37aef95e90575f6c96653ad85d24a9057fb
pedritusz <pedritusz@gmail.com>
2020-06-19 16:42:39 +0200

    log error0.1

--- config/constatants.js
+++ config/constatants.js
@@ -1,3 +1,3 @@
 module.exports.SEED = 'app create by pedritusZ';
 module.exports.TIMETOKEN = 145673;
-module.exports.MONGO_URI = 'mongodb://<dbuser>:<dbpassword>@ds135207.mlab.com:35207/heroku_z3zsq5vv' || 'mongodb://localhost:27017/MiNeveraDB';
+module.exports.MONGO_URI = 'mongodb://@ds135207.mlab.com:35207/heroku_z3zsq5vv' || 'mongodb://localhost:27017/MiNeveraDB';
