767dea0aa059aefbc48a7be8fb531e62fbbf4612
pedritusz <pedritusz@gmail.com>
2020-06-19 16:27:23 +0200

    log error

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParse.urlencoded({extended:false}));
 app.use(bodyParse.json());
 //mongose config
 mongoose.connection.openUri(mongooUri,(err,res)=>{
-    if(err) console.log('error')
+    if(err) console.log('error al conectar con mongo')
     if(res) console.log('connect')
 })
 
